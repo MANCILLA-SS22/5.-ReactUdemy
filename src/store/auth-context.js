8f8352b14549a22210d3060d829992bdc5ddc5e1
MANCILLA-SS22 <xxel.tiradorxx@gmail.com>
2023-09-30 13:28:51 -0700

    first commit

---
+++ src/store/auth-context.js
@@ -0,0 +1,37 @@
+import React, { createContext,  useState, useEffect } from 'react';
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: function(){},
+  onLogin: function (email, password){}
+});
+
+export function AuthContextProvider(props){
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => { //We don't wanna run directly the code inside the useEffect in the component funcion, because now this funcion is executed by react and it is executed after every component re-evaluation. So, whenever App component ran thereafter, the useEffect will run, and if we update setIsLoggedIn(true), the component will run again. But this useEffect won't just run after every component evaluation, but only if the dependencies array changed. For the first time, this hook will forcibly be executed, and for the next render, we'll need to specify when this should be executed again.
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true); // We should of course check email and password. But it's just a dummy/ demo anyways
+    }
+  }, []);
+
+  function logoutHandler() {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  function loginHandler() {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler}}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
